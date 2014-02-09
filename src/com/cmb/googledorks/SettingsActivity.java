8d0e617893d85b18bf5dd7591166db8b40f8cb77
Chris Baughman <cmbaughman@outlook.com>
2014-02-09 10:05:07 -0500

    add missing files

---
+++ src/com/cmb/googledorks/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.cmb.googledorks;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.view.View.*;
+
+public class SettingsActivity extends Activity
+{
+	
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings_activity);
+		
+	}
+}
